789a0bc80a8c4e3c5ec06e4ea4f6fc278236c9ea
Hank Shen <swhbox@foxmail.com>
2020-07-06 12:32:01 +0800

    update

---
+++ upload/driver/vditor/markdown.go
@@ -0,0 +1,97 @@
+/*
+
+   Copyright 2016 Wenhui Shen <www.webx.top>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+package vditor
+
+import (
+	"encoding/json"
+
+	uploadClient "github.com/webx-top/client/upload"
+	"github.com/webx-top/echo"
+)
+
+func init() {
+	uploadClient.Register(`markdown`, func() uploadClient.Client {
+		return New()
+	})
+}
+
+func New() uploadClient.Client {
+	client := &Vditor{}
+	client.BaseClient = uploadClient.New(client)
+	return client
+}
+
+type Vditor struct {
+	*uploadClient.BaseClient
+}
+
+func (a *Vditor) Name() string {
+	return "file[]"
+}
+
+type Response struct {
+	Msg  string      `json:"msg"`
+	Code int         `json:"code"`
+	Data interface{} `json:"data"`
+}
+
+type ResponseUpload struct {
+	ErrFiles []string          `json:"errFiles"`
+	SuccMap  map[string]string `json:"succMap"`
+
+	// for save remote image
+	OriginalURL string `json:"originalURL"`
+	URL         string `json:"url"`
+}
+
+/*
+{
+ "msg": "",
+ "code": 0,
+ "data": {
+ "errFiles": ['filename', 'filename2'],
+ "succMap": {
+   "filename3": "filepath3",
+   "filename3": "filepath3"
+   }
+ }
+}
+*/
+
+func (a *Vditor) Result() (r string) {
+	resp := &Response{}
+	uploadResult := &ResponseUpload{}
+	if a.GetError() != nil {
+		uploadResult.ErrFiles = append(uploadResult.ErrFiles, a.Data.FileName)
+		resp.Msg = a.GetError().Error()
+		resp.Code = 1
+	} else {
+		uploadResult.SuccMap = map[string]string{
+			a.Data.FileName: a.Data.FileURL,
+		}
+		if m, y := a.Data.Addon.(echo.H); y {
+			uploadResult.OriginalURL = m.String(`originalURL`)
+			uploadResult.URL = a.Data.FileURL
+		}
+	}
+	resp.Data = uploadResult
+	b, _ := json.Marshal(resp)
+	r = string(b)
+	return
+}
