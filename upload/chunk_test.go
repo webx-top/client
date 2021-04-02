c3058f3e84cd1bd766aafaf30dd33854ec4930a2
Wenhui Shen <swhbox@foxmail.com>
2021-04-02 10:52:16 +0800

    improved

--- upload/chunk_test.go
+++ upload/chunk_test.go
@@ -13,13 +13,25 @@ import (
 	"path/filepath"
 	"sync"
 	"testing"
+	"time"
 
+	"github.com/admpub/log"
 	"github.com/webx-top/echo/testing/test"
 )
 
-func TestChunkUpload(t *testing.T) {
-	os.RemoveAll("../_testdata")
-	path := "../_testdata/test.txt" //要上传文件所在路径
+func testChunkUpload(t *testing.T, graduallyMerge bool, asyncMergeAll ...bool) {
+	log.SetLevel(`Warn`)
+	log.Sync()
+	var subdir string
+	if graduallyMerge {
+		subdir = `/graduallyMerge`
+	} else if len(asyncMergeAll) > 0 && asyncMergeAll[0] {
+		subdir = `/asyncMergeAll`
+	} else {
+		subdir = `/syncMergeAll`
+	}
+	os.RemoveAll("../_testdata" + subdir)
+	path := "../_testdata" + subdir + "/test.txt" //要上传文件所在路径
 	os.MkdirAll(filepath.Dir(path), os.ModePerm)
 	var file *os.File
 	var err error
@@ -29,8 +41,8 @@ func TestChunkUpload(t *testing.T) {
 			t.Error(err)
 		}
 
-		for i := 1; i <= 150; i++ {
-			file.WriteString(fmt.Sprintf("%03d github.com/webx-top/client\n", i))
+		for i := 1; i <= 1500; i++ {
+			file.WriteString(fmt.Sprintf("%05d github.com/webx-top/client\n", i))
 		}
 	} else {
 		file, err = os.Open(path)
@@ -45,12 +57,17 @@ func TestChunkUpload(t *testing.T) {
 	chunks := 15
 	chunkSize := len(b) / chunks
 	cu := &ChunkUpload{
-		TempDir: `../_testdata/tmp`,
-		SaveDir: `../_testdata/upload/`,
+		TempDir:        `../_testdata` + subdir + `/tmp`,
+		SaveDir:        `../_testdata` + subdir + `/upload/`,
+		GraduallyMerge: graduallyMerge,
+	}
+	if len(asyncMergeAll) > 0 {
+		cu.SetAsyncMerge(asyncMergeAll[0])
 	}
 	wg := &sync.WaitGroup{}
 	wg.Add(chunks)
 	upload := func(r io.Reader, chunkIndex int) {
+		//chunkStartTime := time.Now()
 		body := &bytes.Buffer{}
 		writer := multipart.NewWriter(body)
 		part, err := writer.CreateFormFile("file", filepath.Base(path))
@@ -78,14 +95,16 @@ func TestChunkUpload(t *testing.T) {
 
 		t.Log(res.Body.String())
 		wg.Done()
+		//log.Warn(subdir + ` chunk(` + fmt.Sprintf(`%d`, chunkIndex) + `) elapsed: ` + time.Since(chunkStartTime).String())
 	}
+	startTime := time.Now()
 	file.Seek(0, 0)
 	for i := 0; i < chunks; i++ {
 		if i == chunks-1 {
 			chunkSize = len(b) - chunkSize*(chunks-1)
 		}
 		data := make([]byte, chunkSize)
-		fmt.Printf("offset: %d (%d)\n", i*chunkSize, i*chunkSize+chunkSize)
+		//fmt.Printf("offset: %d (%d)\n", i*chunkSize, i*chunkSize+chunkSize)
 		n, err := file.Read(data)
 		if err == io.EOF {
 			wg.Done()
@@ -95,6 +114,7 @@ func TestChunkUpload(t *testing.T) {
 		go upload(buf, i)
 	}
 	wg.Wait()
+	log.Warn(subdir + ` elapsed: ` + time.Since(startTime).String())
 	uploaded, err := ioutil.ReadFile(cu.GetSavePath())
 	/*
 		if err != nil || string(uploaded) != string(b) {
@@ -106,4 +126,16 @@ func TestChunkUpload(t *testing.T) {
 	test.Eq(t, nil, err)
 	test.Eq(t, string(b), string(uploaded))
 	//os.RemoveAll("../_testdata")
+}
+
+func TestChunkUploadAsyncMergeAll(t *testing.T) {
+	testChunkUpload(t, false, true)
+}
+
+func TestChunkUploadSyncMergeAll(t *testing.T) {
+	testChunkUpload(t, false, false)
+}
+
+func TestChunkUploadGraduallyMerge(t *testing.T) {
+	testChunkUpload(t, true)
 }
